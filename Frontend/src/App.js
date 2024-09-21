7b24a6933d3f340161ac5c0b81b89255fa651ea0
markpakkawat <pakkawatwassa@gmail.com>
2024-09-21 12:35:43 +0700

    Profile page's surface
    only CSS have been done

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 import './assets/css/App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path='/profile' element={<Profile />}/>
         </Routes>
       </div>
     </Router>
