20ad925d57793f8e05819109a841bf1a880ed696
Micko Kok <kokmicko@gmail.com>
2024-09-28 17:18:02 +0700

    Update CreatePost.js

--- frontend/src/pages/CreatePost.js
+++ frontend/src/pages/CreatePost.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
 import TextEditor from '../components/TextEditor';
 import '../assets/css/index.css'; // Ensure you have Tailwind's CSS imported
 
 const CreatePost = () => {
+  // Managing selected radio button option using useState
+  const [selectedOption, setSelectedOption] = useState("option1");
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
-    <div className="flex flex-wrap">
+    <div className="flex h-screen w-full">
       {/* //division 1: return to post feed page */}
       <div className="border w-1/12 mt-4">
         <button 
           type="button" 
-          className=" mr-4 btn bg-purple-600 bg-opacity-100 text-white hover:bg-opacity-80 transition"
+          className="mr-4 btn bg-purple-600 bg-opacity-100 text-white hover:bg-opacity-80 transition"
         >
           Primary
         </button>
       </div>
       {/* division 2: text editor + post type picker + post button */}
-      <div className="border w-8/12 mt-4">
+      <div className="border w-8/12 h-screen mt-4">
         <div className="mt-4">
           <input
             type="text"
@@ -28,27 +35,26 @@ const CreatePost = () => {
           <TextEditor />
         </div>
 
-        <div>
-          <button 
-            type="button" 
-            className=" mr-4 btn bg-purple-600 bg-opacity-100 text-white hover:bg-opacity-80 transition"
-          >
-            Discussion
-          </button>
-          <button 
-            type="button" 
-            className=" mr-4 btn bg-purple-600 bg-opacity-100 text-white hover:bg-opacity-80 transition"
-          >
-            Advice and Feedback
-          </button>
-        </div>
-        <div>
-          <button 
-            type="button" 
-            className=" mr-4 btn bg-purple-600 bg-opacity-100 text-white hover:bg-opacity-80 transition"
-          >
-            Post
-          </button>
+        <div className='flex'>
+          <div class="w-6/12 inline-flex rounded-md shadow-sm" role="group">
+            <button type="button" class="px-4 py-2 text-sm font-medium text-white bg-purple-600 border border-gray-900 rounded-s-lg focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+              Select Post Type
+            </button>
+            <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+              Discussion
+            </button>
+            <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+              Advice and Feedback
+            </button>
+          </div>
+          <div className='w-6/12'> 
+            <button 
+              type="button" 
+              className=" mr-4 btn bg-purple-600 bg-opacity-100 text-white hover:bg-opacity-80 transition"
+            >
+              Post
+            </button>
+          </div>
         </div>
       </div>
       {/* //division 2: writing assistant + dashboard */}
