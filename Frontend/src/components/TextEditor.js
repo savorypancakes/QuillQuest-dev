9942b08c5c9b28eecc9cd9c46daa953ad694bfb6
Micko Kok <kokmicko@gmail.com>
2024-10-03 14:52:30 +0700

    Update

--- Frontend/src/components/TextEditor.js
+++ Frontend/src/components/TextEditor.js
@@ -1,49 +1,48 @@
 // src/Tiptap.tsx
+import React, { useEffect, useState } from 'react';
 import { useEditor, EditorContent } from '@tiptap/react';
 import Document from '@tiptap/extension-document';
 import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
-import Placeholder from '@tiptap/extension-placeholder'; // Import the placeholder extension
-import '../assets/css/components/TextEditor.css'; // Assuming you have styles in this file
-import React, { useState , useEffect } from 'react'
+import Placeholder from '@tiptap/extension-placeholder';
+import '../assets/css/components/TextEditor.css';
 
 const TextEditor = ({ initialContent, onContentChange, className = '', editorStyles }) => {
-  
   const [content, setContent] = useState(initialContent);
-  
+
   const editor = useEditor({
     extensions: [
       Document,
       Paragraph,
       Text,
       Placeholder.configure({
-        placeholder: 'Untitled document', // Set your placeholder text here
+        placeholder: 'Untitled document',
       }),
     ],
-    content: content, // Start with empty content
+    content: content,
     onUpdate: ({ editor }) => {
-      const newContent = editor.getText();
+      const newContent = editor.getHTML();
       setContent(newContent);
       onContentChange(newContent);
     },
-  })
-  
+  });
+
   useEffect(() => {
     if (editor && initialContent !== '' && content === '') {
       editor.commands.setContent(initialContent);
       setContent(initialContent);
     }
   }, [editor, initialContent, content]);
-  
+
   if (!editor) {
-    return null // Render nothing if editor is not ready
+    return null;
   }
 
   return (
     <div className={`text-editor-wrapper ${className}`} style={editorStyles}>
       <EditorContent editor={editor} />
     </div>
-  )
-}
+  );
+};
 
-export default TextEditor
+export default TextEditor;
